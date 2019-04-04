c6c4d710ea1edc0a3681fcce19392cbe8f56c19f
CarolinJohnny <carolinjohnny@googlemail.com>
2019-04-04 20:24:00 +0200

    README, js, css added

---
+++ README.md
@@ -0,0 +1,7 @@
+# Modal PopUp
+
+This Modal PopUp is made with CSS and JavaScript.
+
+#### Use
+
+It can be used to display results of a game. It opens and closes by clicking a button.
---
+++ app.js
@@ -0,0 +1,23 @@
+let modal = document.querySelector(".modal");
+let results = document.querySelector(".results");
+let againButton = document.querySelector(".again-button");
+
+function toggleModal() {
+  modal.classList.toggle("show-modal");
+}
+
+function windowOnClick(event) {
+  if (event.target === modal) {
+    toggleModal();
+  }
+}
+
+results.addEventListener("click", toggleModal);
+againButton.addEventListener("click", toggleModal);
+window.addEventListener("click",windowOnClick);
+
+//
+var totalStars = document.querySelector(".stars").innerHTML;
+gamerMoves.innerHTML = moves
+document.getElementById("moves").innerHTML = moves;
+document.getElementById("totalStars").innerHTML = totalStars;
